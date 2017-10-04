97a2eb4e8054610fc03027163ce6fcb8d25078a8
Gabriel Courtemanche <gabriel.courtemanche@gmail.com>
2017-10-03 20:37:01 -0400

    boilerplate

---
+++ src/components/LoginForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <Text>
+        LOTUS
+      </Text>
+    );
+  }
+}
+
+export default LoginForm;
