f0c5fa71b8186601e35efc39574eee718d03b0d6
Gabriel Courtemanche <gabriel.courtemanche@gmail.com>
2017-10-31 14:26:41 -0400

    Login form and common components

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, Image, View } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
   render() {
     return (
-      <Text>
-        LOTUS
-      </Text>
+      <View>
+        <Card>
+          <CardSection>
+            <Input
+              label="Courriel"
+              placeholder="nom@domaine.com"
+              value={this.props.email}
+            />
+          </CardSection>
+
+          <CardSection>
+            <Input
+              label="Mot de passe"
+              placeholder="********"
+              value={this.props.password}
+            />
+          </CardSection>
+
+          <CardSection>
+            <Button>
+              Connexion
+            </Button>
+          </CardSection>
+
+        </Card>
+
+        <View style={{ paddingTop: 40 }}>
+          <Image
+            style={styles.imageStyle}
+            source={require('./logo.png')}
+          />
+        </View>
+      </View>
     );
   }
 }
+
+const styles = {
+  imageStyle: {
+    height: 300,
+    width: 300,
+    alignSelf: 'center'
+  }
+};
 
 export default LoginForm;
